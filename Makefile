720dfcc9640042005ec3ac3cd418d4d01944d277
Martin Burak Brettschneider <mbrettsc@k2m03s03.42kocaeli.com.tr>
2023-12-15 18:17:41 +0300

    updated

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all:
-	mkdir -p /home/data /home/data/mysql /home/data/wordpress
+	mkdir -p /home/mbrettsc/data /home/mbrettsc/data/mysql /home/mbrettsc/data/wordpress
 	docker-compose -f ./srcs/docker-compose.yml up --build -d
 down:
 	docker-compose -f ./srcs/docker-compose.yml down
 clean: down
 	docker system prune -a
 	docker volume rm $$(docker volume ls -q)
-	rm -rf /home/data
+	rm -rf /home/mbrettsc/data
 re: down clean all
--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -12,7 +12,8 @@ sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 sed -i "s/database_name_here/$MYSQL_DB_NAME/g" wp-config-sample.php
 mv wp-config-sample.php wp-config.php
 
-wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=mbrettsc@student.42kocaeli.com.tr --allow-root
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_EMAIL --allow-root
+wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_PASSWORD --allow-root
 wp theme install hestia --activate --allow-root
 
 exec "$@"
